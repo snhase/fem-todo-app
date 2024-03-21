d711615fff6edcd49f1b46ead89d7c5a49bee645
snhase <snehal.hase@gmail.com>
2024-03-20 21:45:09 -0700

    add configurations for typescript and tailwind

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className="mx-auto h-screen">
+      <header className="h-1/3 bg-[url('./assets/images/bg-desktop-light.jpg')] bg-cover">
+        <div className="flex justify-between">
+          <h1 className="mx-auto pt-10 text-center uppercase text-3xl font-extrabold text-white"> T&nbsp;&nbsp;O&nbsp;&nbsp;D&nbsp;&nbsp;O</h1>
+        </div>
+      </header>
+      <main className="mx-auto">
+
+      </main>
+    </div>
+  );
+}
+
+export default App;
